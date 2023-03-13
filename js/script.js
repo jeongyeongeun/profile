55c9354f20945a4e101ba4334be386d6d46771f1
jeongyeongeun <a0257942@gmail.com>
2023-03-13 23:58:16 +0900

    mainFunc

--- js/script.js
+++ js/script.js
@@ -1,52 +1,23 @@
+let isScroll = false;
+const nav = ".navbar";
+
 $(function(){
     // As A jQuery Plugin
-$('#demo').gradient({
-    colors: ['#a960ee', '#ff333d', '#90e0ff', '#ffcb57'],
-    height: '1000'
+    $('#demo').gradient({
+        colors: ['#a960ee', '#ff333d', '#90e0ff', '#ffcb57'],
+        height: '1000'
     });
-    //시간지나면 스크롤내리기
-    let lnb3 = $(".navbar").offset().top; // 0
 
-    // $(window).on({
-    //     load : setTimeout(function(){
-    //         $(this).scrollTop(900)
-    //     },3000),
-    //     scroll : function(){
-    //         let disTop = $(window).scrollTop();
-    //         if(lnb3 <= disTop){ 
-    //             console.log("22")
-    //             $(window).off("load")
-    //             console.log("됨?")
-    //             return false;
-    //         }else{
-    //             // $(".option_wrap").removeClass('fixN');
-    //         }
-            
-    //     }
-    // });
-    $(window).on("load",function(){
-        console.log(1)
-    })
+    f_scroll();
+
     setTimeout(function(){
-        let disTop = $(window).scrollTop();
-        location.href = "#about"
-        if(lnb3<disTop){
-            console.log("???")
-            return false;
+        console.log(isScroll)
+        if(isScroll == 0){
+            location.href = "#about"
         }
-    },16000)
+    }, 5000)
 
-        // $(window).on("scroll",function(){
-        //     let disTop = $(window).scrollTop();
-        //     if(lnb3 <= disTop){ 
-        //         console.log("22")
-        //         setTimeout(function(){
-        //             $(window).scrollTop()
-        //         },10000000)
-        //         console.log("됨?")
-        //         return false;
-        //     }
-        // })
+    $(window).on("scroll", f_scroll)
 
         //#topBtn 마우스오버시 글씨바뀜
         $("#topBtn").on({
@@ -82,3 +53,15 @@ $('#demo').gradient({
 
 
 })
+
+const f_isScroll = (element) => {
+    return ($(element).offset().top <= $(window).scrollTop());
+}
+
+const f_scroll = () => {
+    if(f_isScroll(nav)){     //스크롤 했을 때
+        isScroll = true;
+        return;
+    }
+    isScroll = false;
+}
