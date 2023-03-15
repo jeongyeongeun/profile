c4333877edb74bf1d6222b4d91be3025486c3793
jeongyeongeun <a0257942@gmail.com>
2023-03-15 23:55:10 +0900

    wheel

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 let isScroll = false;
-const nav = ".navbar";
+
+window.addEventListener("wheel", function(e){
+    e.preventDefault();
+},{passive : false});
+
 $(function(){
     // As A jQuery Plugin
     $('#demo').gradient({
@@ -7,19 +11,19 @@ $(function(){
         height: '1000'
     });
 
-    f_scroll();
+    // f_scroll();
 
     setTimeout(function(){
         console.log(isScroll)
         if(isScroll == 0){
-            location.replace('#about');
-            // $('html, body').animate({
-            //     scrollTop: (document.getElementById('about').getBoundingClientRect().height)
-            //   }, 400);
+            console.log("1")
+            page = 2
+            var posTop =(page-1) * $(window).height();
+            mHtml.animate({scrollTop : posTop}, 500);
         }
     }, 5000)
 
-    $(window).on("scroll", f_scroll)
+    // $(window).on("scroll", f_scroll)
 
         //#topBtn 마우스오버시 글씨바뀜
         $("#topBtn").on({
@@ -33,6 +37,9 @@ $(function(){
                 $("span",this).text("Top")
             }
         })
+        const mHtml = $("html");
+        var page = 1;
+        mHtml.animate({scrollTop : 0},100);
         $(window).on({
             scroll:function(){
                 if ($(this).scrollTop() > 250) {
@@ -41,14 +48,29 @@ $(function(){
                     $("#topBtn").fadeOut();
                     $("#topBtn span").text("Top")
                 }
+            },
+            wheel: function(e){
+                if(mHtml.is(":animated")) return;
+                if(e.originalEvent.deltaY > 0) {
+                    isScroll = true;
+                    if(page == 12) return;
+                    page++;
+                } else if(e.originalEvent.deltaY < 0) {
+                    if(page == 1) return;
+                    page--;
+                } 
+                if(page == 1) isScroll = false;
+                var posTop =(page-1) * $(window).height();
+                mHtml.animate({scrollTop : posTop}, 500);
             }
         })
         $("#topBtn").on("click",function() {
+            page = 1;
+            isScroll = false;
             $('html').animate({
                 scrollTop : 0
             }, 600);
-            $("#topBtn span").text("슈웅")
-            return false;
+            $("#topBtn span").text("슈웅");
         });
 
         //텍스트 쪼개서 서로 다른 색 나오게 하기
@@ -57,22 +79,12 @@ $(function(){
         
         //     return '<span>' + chars.join('</span><span>') + '</span>';
         // });
+        //
+
+        
+        
 
 
 
 
 })
-
-const f_isScroll = () => {
-    let scrollY = $(window)[0].scrollY;
-    return 0 < scrollY;
-}
-
-const f_scroll = () => {
-    if(f_isScroll()){     //스크롤 했을 때
-        isScroll = true;
-        return;
-    }
-    isScroll = false;
-}
-
