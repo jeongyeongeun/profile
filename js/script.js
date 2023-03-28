a3194cf30c69392c6c07fc3d0133a7ff837c2e98
jeongyeongeun <a0257942@gmail.com>
2023-03-29 02:38:41 +0900

    mainPage

--- js/script.js
+++ js/script.js
@@ -5,11 +5,7 @@ let isScroll = false;
 // },{passive : false});
 
 $(function(){
-    // As A jQuery Plugin
-    // $('#demo').gradient({
-    //     colors: ['#a960ee', '#ff333d', '#90e0ff', '#ffcb57'],
-    //     height: '1000'
-    // });
+
 
     // f_scroll();
 
@@ -21,22 +17,22 @@ $(function(){
             var posTop =(page-1) * $(window).height();
             mHtml.animate({scrollTop : posTop}, 500);
         }
-    }, 5000)
+    }, 2500)
 
     // $(window).on("scroll", f_scroll)
 
         //#topBtn 마우스오버시 글씨바뀜
-        $("#topBtn").on({
-            mousedown: function(){
-                $("span",this).text("슝")
-            },
-            mouseover: function(){
-                $("span",this).text("슝")
-            },
-            mouseout: function(){
-                $("span",this).text("Top")
-            }
-        })
+        // $("#topBtn").on({
+        //     mousedown: function(){
+        //         $("span",this).text("슝")
+        //     },
+        //     mouseover: function(){
+        //         $("span",this).text("슝")
+        //     },
+        //     mouseout: function(){
+        //         $("span",this).text("Top")
+        //     }
+        // })
         const mHtml = $("html");
         var page = 1;
         mHtml.animate({scrollTop : 0},100);
@@ -116,4 +112,77 @@ $(function(){
 
 
 
+        const canvas = document.getElementById("particles");
+        if (canvas.getContext){
+            var ctx = canvas.getContext('2d');
+            // drawing code here
+          } else {
+            console.log("no")
+            // canvas-unsupported code here
+          }
+        // const ctx = canvas.getContext("2d");
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        
+        const particlesArray = [];
+        
+        class Particle {
+            constructor() {
+                this.x = Math.random() * canvas.width;
+                this.y = Math.random() * canvas.height;
+                this.size = Math.random() * 5 + 1;
+                this.speedX = Math.random() * 3 - 1.5;
+                this.speedY = Math.random() * 3 - 1.5;
+                this.color = `hsl(${Math.random() * 360}, 100%, 50%)`;
+            }
+        
+            update() {
+                this.x += this.speedX;
+                this.y += this.speedY;
+        
+                if (this.size > 0.2) this.size -= 0.1;
+                if (this.x < 0 || this.x > canvas.width) this.speedX *= -1;
+                if (this.y < 0 || this.y > canvas.height) this.speedY *= -1;
+            }
+        
+            draw() {
+                ctx.fillStyle = this.color;
+                ctx.beginPath();
+                ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+                ctx.closePath();
+                ctx.fill();
+            }
+        }
+        
+        function handleParticles() {
+            for (let i = 0; i < particlesArray.length; i++) {
+                particlesArray[i].update();
+                particlesArray[i].draw();
+        
+                if (particlesArray[i].size <= 0.2) {
+                    particlesArray.splice(i, 1);
+                    i--;
+                }
+            }
+        }
+        
+        function createParticles() {
+            if (particlesArray.length < 100) {
+                particlesArray.push(new Particle());
+            }
+        }
+        
+        function animateParticles() {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            handleParticles();
+            createParticles();
+            requestAnimationFrame(animateParticles);
+        }
+        
+        animateParticles();
+        
+        window.addEventListener("resize", () => {
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+        });
 })
